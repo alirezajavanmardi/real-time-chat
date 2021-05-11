d9e249e56cace8a7588866c7ed2b5f8ea2cc3145
Alireza javanmardi <alizzjavan77@gmail.com>
2021-05-11 19:28:34 +0430

    add users update send data to a client

--- src/app.js
+++ src/app.js
@@ -4,11 +4,35 @@ const ws = require("ws");
 const server = http.createServer(express);
 const port = 8080;
 const wsServer = new ws.Server({ server });
-
+const user = [
+  {
+    phone: "09107122282",
+    userName: "alizzjvn1",
+  },
+  {
+    phone: "09107122283",
+    userName: "alizzjvn2",
+  },
+  {
+    phone: "09107122284",
+    userName: "alizzjvn3",
+  },
+];
 wsServer.on("connection", (wss) => {
-  wss.on("message", (message) => {
+  wss.on("message", (input) => {
     wsServer.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === ws.OPEN) client.send(message);
+      if (client !== ws && client.readyState === ws.OPEN) {
+        const data = JSON.parse(input);
+        user.map((user_data) => {
+          if (user_data.phone == data.phone) {
+            return client.send(JSON.stringify({
+              data:data,
+              user:user_data
+            }));
+          }
+        });
+        
+      }
     });
   });
 });
